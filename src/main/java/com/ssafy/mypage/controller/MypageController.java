ba460b5ca25f96fab0a571969d1e5709ddaeb22b
gunhoo <gunhoo2016@gmail.com>
2023-05-19 15:43:40 +0900

    feat: finish mypage controller

--- src/main/java/com/ssafy/mypage/controller/MypageController.java
+++ src/main/java/com/ssafy/mypage/controller/MypageController.java
@@ -1,5 +1,6 @@
 package com.ssafy.mypage.controller;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.slf4j.Logger;
@@ -9,15 +10,17 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.ssafy.board.model.BoardDto;
-import com.ssafy.board.model.BoardParameterDto;
 import com.ssafy.board.model.service.BoardService;
 import com.ssafy.hotplace.model.HotplaceDto;
 import com.ssafy.hotplace.model.service.HotplaceService;
+import com.ssafy.like.model.LikeDto;
 import com.ssafy.mypage.model.service.MypageService;
 import com.ssafy.plan.model.PlanDto;
 import com.ssafy.plan.model.service.PlanService;
@@ -45,10 +48,11 @@ public class MypageController {
 		this.planService = planService;
 	}
 	
-	@GetMapping("/article/board")
+	@PostMapping("/board")
 	@ApiOperation(value = "내가 쓴 게시판 목록을 반환한다.", response = List.class)
-	private ResponseEntity<?> listBoard(@ApiParam(value = "내가 쓴 게시글을 얻기위해 현재 로그인한 user id 정보.", required = true) String userId) {
+	private ResponseEntity<?> listBoard(@RequestBody @ApiParam(value = "내가 쓴 게시글을 얻기위해 현재 로그인한 user id 정보.", required = true) String userId) {
 		logger.debug("Mypage("+userId+") : listBoard call");
+		userId = userId.substring(1, userId.length()-1);
 		try {
 			List<BoardDto> list = boardService.listMyArticle(userId);
 			return new ResponseEntity<List<BoardDto>>(list, HttpStatus.OK);
@@ -57,10 +61,11 @@ public class MypageController {
 		}
 	}
 	
-	@GetMapping("/article/hotplace")
+	@PostMapping("/hotplace")
 	@ApiOperation(value = "내가 쓴 게시판 목록을 반환한다.", response = List.class)
-	private ResponseEntity<?> listHotplace(@ApiParam(value = "내가 쓴 핫플레이스를 얻기위해 현재 로그인한 user id 정보.", required = true) String userId) {
+	private ResponseEntity<?> listHotplace(@RequestBody @ApiParam(value = "내가 쓴 핫플레이스를 얻기위해 현재 로그인한 user id 정보.", required = true) String userId) {
 		logger.debug("Mypage("+userId+") : listHotplace call");
+		userId = userId.substring(1, userId.length()-1);
 		try {
 			List<HotplaceDto> list = hotplaceService.listMyHotplace(userId);
 			return new ResponseEntity<List<HotplaceDto>>(list, HttpStatus.OK);
@@ -69,10 +74,11 @@ public class MypageController {
 		}
 	}
 	
-	@GetMapping("/article/plan")
+	@PostMapping("/plan")
 	@ApiOperation(value = "내가 쓴 여행계획 목록을 반환한다.", response = List.class)
-	private ResponseEntity<?> listPlan(@ApiParam(value = "내가 쓴 여행계획을 얻기위해 현재 로그인한 user id 정보.", required = true) String userId) {
+	private ResponseEntity<?> listPlan(@RequestBody @ApiParam(value = "내가 쓴 여행계획을 얻기위해 현재 로그인한 user id 정보.", required = true) String userId) {
 		logger.debug("Mypage("+userId+") : listPlan call");
+		userId = userId.substring(1, userId.length()-1);
 		try {
 			List<PlanDto> list = planService.listMyPlan(userId);
 			return new ResponseEntity<List<PlanDto>>(list, HttpStatus.OK);
@@ -82,4 +88,21 @@ public class MypageController {
 	}
 	
 	//TODO: /like 메서드 구현
+	@PostMapping("/like")
+	@ApiOperation(value = "내가 좋아요 누른 핫플 목록을 반환한다.", response = List.class)
+	private ResponseEntity<?> listLike(@RequestBody @ApiParam(value = "내가 좋아요 한 핫플 리스트를 얻기위해 현재 로그인한 user id 정보.", required = true) String userId) {
+		logger.debug("Mypage("+userId+") : listLike call");
+		userId = userId.substring(1, userId.length()-1);
+		try {
+			List<LikeDto> list = hotplaceService.listLike(userId);
+			List<HotplaceDto> likedHotplace = new ArrayList<>();
+			for(LikeDto id : list) {
+				HotplaceDto item = hotplaceService.viewHotplace(""+id.getHotplaceId());
+				likedHotplace.add(item);
+			}
+			return new ResponseEntity<List<HotplaceDto>>(likedHotplace, HttpStatus.OK);
+		} catch (Exception e) {
+			return new ResponseEntity<Void>(HttpStatus.NO_CONTENT);
+		}
+	}
 }
