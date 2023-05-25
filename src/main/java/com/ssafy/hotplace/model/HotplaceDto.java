fadd1993409feb682a1b2d622c0a420b6f2088f4
ljh8190 <ljh8190@naver.com>
2023-05-25 11:16:09 +0900

    modify: view user profile image

--- src/main/java/com/ssafy/hotplace/model/HotplaceDto.java
+++ src/main/java/com/ssafy/hotplace/model/HotplaceDto.java
@@ -14,6 +14,7 @@ public class HotplaceDto {
 	private String mapUrl;
 	private boolean like = false;
 	private int likeCnt;
+	private String userImage;
 	
 	public HotplaceDto() {}
 	
@@ -31,91 +32,118 @@ public class HotplaceDto {
 		this.mapUrl = mapUrl;
 	}
 	
-	
+	public String getUserId() {
+		return userId;
+	}
+
+	public void setUserId(String userId) {
+		this.userId = userId;
+	}
+
+	public int getNum() {
+		return num;
+	}
+
+	public void setNum(int num) {
+		this.num = num;
+	}
+
+	public String getImage() {
+		return image;
+	}
+
+	public void setImage(String image) {
+		this.image = image;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getJoinDate() {
+		return joinDate;
+	}
+
+	public void setJoinDate(String joinDate) {
+		this.joinDate = joinDate;
+	}
+
+	public String getDesc() {
+		return desc;
+	}
+
+	public void setDesc(String desc) {
+		this.desc = desc;
+	}
+
+	public String getTag1() {
+		return tag1;
+	}
+
+	public void setTag1(String tag1) {
+		this.tag1 = tag1;
+	}
+
+	public String getTag2() {
+		return tag2;
+	}
+
+	public void setTag2(String tag2) {
+		this.tag2 = tag2;
+	}
+
+	public double getLatitude() {
+		return latitude;
+	}
+
+	public void setLatitude(double latitude) {
+		this.latitude = latitude;
+	}
+
+	public double getLongitude() {
+		return longitude;
+	}
+
+	public void setLongitude(double longitude) {
+		this.longitude = longitude;
+	}
+
+	public String getMapUrl() {
+		return mapUrl;
+	}
+
+	public void setMapUrl(String mapUrl) {
+		this.mapUrl = mapUrl;
+	}
+
+	public boolean isLike() {
+		return like;
+	}
+
+	public void setLike(boolean like) {
+		this.like = like;
+	}
+
 	public int getLikeCnt() {
 		return likeCnt;
 	}
-
 
 	public void setLikeCnt(int likeCnt) {
 		this.likeCnt = likeCnt;
 	}
 
+	public String getUserImage() {
+		return userImage;
+	}
 
-	public boolean getLike() {
-		return like;
+	public void setUserImage(String userImage) {
+		this.userImage = userImage;
 	}
-	public void setLike(boolean like) {
-		this.like = like;
-	}
-	public String getUserId() {
-		return userId;
-	}
-	public void setUserId(String userId) {
-		this.userId = userId;
-	}
-	public int getNum() {
-		return num;
-	}
-	public void setNum(int num) {
-		this.num = num;
-	}
-	public String getImage() {
-		return image;
-	}
-	public void setImage(String image) {
-		this.image = image;
-	}
-	public String getTitle() {
-		return title;
-	}
-	public void setTitle(String title) {
-		this.title = title;
-	}
-	public String getJoinDate() {
-		return joinDate;
-	}
-	public void setJoinDate(String joinDate) {
-		this.joinDate = joinDate;
-	}
-	public String getDesc() {
-		return desc;
-	}
-	public void setDesc(String desc) {
-		this.desc = desc;
-	}
-	public String gettag1() {
-		return tag1;
-	}
-	public void settag1(String tag1) {
-		this.tag1 = tag1;
-	}
-	public String gettag2() {
-		return tag2;
-	}
-	public void settag2(String tag2) {
-		this.tag2 = tag2;
-	}
-	public double getLatitude() {
-		return latitude;
-	}
-	public void setLatitude(double latitude) {
-		this.latitude = latitude;
-	}
-	public double getLongitude() {
-		return longitude;
-	}
-	public void setLongitude(double longitude) {
-		this.longitude = longitude;
-	}
-	public String getMapUrl() {
-		return mapUrl;
-	}
-	public void setMapUrl(String mapUrl) {
-		this.mapUrl = mapUrl;
-	}
-	
-	
+
 	@Override
 	public String toString() {
 		StringBuilder builder = new StringBuilder();
